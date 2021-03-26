f9426e6c9cbc45d9f407d84c56b10957328d1f35
Lori McCurry <lori.mccurry@gmail.com>
2021-03-26 15:12:40 -0500

    google oauth login

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,32 @@
 import React from "react";
 import Button from "../styles/Auth";
 import { SignInIcon } from "./Icons";
+import {GoogleLogin} from 'react-google-login';
+import { authenticate } from "../utils/api-client";
 
 function GoogleAuth() {
   return (
-    <Button tabIndex={0} type="button">
-      <span className="outer">
-        <span className="inner">
-          <SignInIcon />
-        </span>
-        sign in
-      </span>
-    </Button>
+    <GoogleLogin
+      clientId="574624334344-0opaqk31ttnvudgv3c2a89hr2ulndft4.apps.googleusercontent.com"
+      cookiePolicy="single_host_origin"
+      onSuccess={authenticate}
+      onFailure={authenticate}
+      render={(renderProps) => (
+        <Button
+          tabIndex={0}
+          type="button"
+          onClick={renderProps.onClick}
+          disabled={renderProps.disabled}
+        >
+          <span className="outer">
+            <span className="inner">
+              <SignInIcon />
+            </span>
+            sign in
+          </span>
+        </Button>
+      )}
+    />
   );
 }
 
